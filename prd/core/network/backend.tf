9c5b8e499a4abc00261dbc85bded7fd3b0b1e52c
Joe Terlecki <70246976+joeterlecki@users.noreply.github.com>
2021-03-19 06:51:04 -0400

    Refactor logging bucket 003 (#4)
    * refactor and configuration
    
    * refactor and configuration

--- prd/core/network/backend.tf
+++ prd/core/network/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "tf-state-root-devinitly"
-    key     = "prd/network/terraform.tfstate"
+    key     = "prd/core/network/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
   }
