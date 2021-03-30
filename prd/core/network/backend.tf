2cbf5fa302783fba44fd097a07aff3cceb8648a3
Joe Terlecki <joe.terlecki@devinitly.com>
2021-03-30 07:34:54 -0400

    rebuilt for tf cloud

--- prd/core/network/backend.tf
+++ prd/core/network/backend.tf
@@ -1,8 +1,18 @@
+# terraform {
+#   backend "s3" {
+#     bucket  = "134905014910-tfstate"
+#     key     = "prd/core/network/terraform.tfstate"
+#     region  = "us-east-1"
+#     encrypt = true
+#   }
+# }
+
 terraform {
-  backend "s3" {
-    bucket  = "134905014910-tfstate"
-    key     = "prd/core/network/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
+  backend "remote" {
+    organization = "Devinitly"
+
+    workspaces {
+      name = "root-network-prd"
+    }
   }
 }
